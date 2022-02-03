d18744d428f048051c4dbbf229a1b907e331b6b9
MichaelDiogo <michaeldiogo17@outlook.com>
2022-02-02 22:27:59 -0300

    feat: implementado port para findUsuarioById

--- orcamento-familiar/src/main/java/alura/orcamentofamiliar/usuario/adapter/out/persistence/UsuarioPersistenceAdapter.java
+++ orcamento-familiar/src/main/java/alura/orcamentofamiliar/usuario/adapter/out/persistence/UsuarioPersistenceAdapter.java
@@ -1,14 +1,16 @@
 package alura.orcamentofamiliar.usuario.adapter.out.persistence;
 
+import alura.orcamentofamiliar.usuario.application.port.out.FindUsuarioByIdPort;
 import alura.orcamentofamiliar.usuario.application.port.out.SalvarUsuarioPort;
 import alura.orcamentofamiliar.usuario.domain.Usuario;
+import alura.orcamentofamiliar.util.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class UsuarioPersistenceAdapter implements SalvarUsuarioPort {
+public class UsuarioPersistenceAdapter implements SalvarUsuarioPort, FindUsuarioByIdPort {
 
     private final UsuarioRepository usuarioRepository;
 
@@ -16,5 +18,12 @@ public class UsuarioPersistenceAdapter implements SalvarUsuarioPort {
     public void salvarUsuario(Usuario usuario) {
 
         usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario findUsuarioById(Long id) {
+
+        return usuarioRepository.findById(id)
+                                .orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado"));
     }
 }
