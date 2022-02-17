c638488e88706cbe139c0dc10357cd4b2f9cc36f
MichaelDiogo <michaeldiogo17@outlook.com>
2022-02-16 22:31:51 -0300

    refactror! aplicação de padroes para melhorar a legibilidade do código

--- orcamento-familiar/src/main/java/alura/orcamentofamiliar/receita/adapter/out/persistence/ReceitaPersistenceAdapter.java
+++ orcamento-familiar/src/main/java/alura/orcamentofamiliar/receita/adapter/out/persistence/ReceitaPersistenceAdapter.java
@@ -1,21 +1,18 @@
 package alura.orcamentofamiliar.receita.adapter.out.persistence;
 
-import alura.orcamentofamiliar.despesa.application.port.out.FindDespesasNomesPorCategoriaPort;
-import alura.orcamentofamiliar.despesa.domain.Categoria;
-import alura.orcamentofamiliar.receita.application.port.out.FindTodasAsReceitasPelaDescricaoPort;
-import alura.orcamentofamiliar.receita.application.port.out.ListarReceitasPorMesPort;
 import alura.orcamentofamiliar.receita.application.port.out.*;
 import alura.orcamentofamiliar.receita.domain.Receita;
 import alura.orcamentofamiliar.util.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Transactional
 @Component
@@ -23,11 +20,11 @@ import java.util.Map;
 public class ReceitaPersistenceAdapter implements SalvarReceitaPort,
                                                   ExisteReceitaNoPeriodoPort,
                                                   FindReceitaByIdPort,
-                                                  FindAllReceitasPort,
                                                   DeletarReceitaByIdPort,
                                                   FindTodasAsReceitasPelaDescricaoPort,
                                                   ListarReceitasPorMesPort,
-                                                  FindSomaDasReceitasNoMesPort {
+                                                  FindSomaDasReceitasNoMesPort,
+                                                  FindAllReceitasPort {
 
     private final ReceitaRepository receitaRepository;
 
@@ -51,9 +48,9 @@ public class ReceitaPersistenceAdapter implements SalvarReceitaPort,
     }
 
     @Override
-    public List<Receita> findAllReceitas() {
+    public Page<Receita> findAllReceitas(Pageable pageable) {
 
-        return receitaRepository.findAll();
+        return receitaRepository.findAll(pageable);
     }
 
     @Override
@@ -63,9 +60,9 @@ public class ReceitaPersistenceAdapter implements SalvarReceitaPort,
     }
 
     @Override
-    public List<Receita> listaTodasAsReceitasPorDescricao(String descricao) {
+    public Page<Receita> listaTodasAsReceitasPorDescricao(String descricao, Pageable pageable) {
 
-        return receitaRepository.findByDescricao(descricao);
+        return receitaRepository.findByDescricao(descricao, pageable);
     }
 
     @Override
@@ -79,6 +76,5 @@ public class ReceitaPersistenceAdapter implements SalvarReceitaPort,
 
         return receitaRepository.findSomaReceitasMensais(ano, mes);
     }
-
 
 }
