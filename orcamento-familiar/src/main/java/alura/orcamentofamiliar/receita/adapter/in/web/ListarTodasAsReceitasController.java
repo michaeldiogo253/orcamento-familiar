0757cc9f85e821373e86feed42c2e7299df536db
MichaelDiogo <michaeldiogo17@outlook.com>
2022-02-16 22:33:00 -0300

    refactror! ajustes no controller de lsitar receitas para adicionar paginação nas consultas

--- orcamento-familiar/src/main/java/alura/orcamentofamiliar/receita/adapter/in/web/ListarTodasAsReceitasController.java
+++ orcamento-familiar/src/main/java/alura/orcamentofamiliar/receita/adapter/in/web/ListarTodasAsReceitasController.java
@@ -1,41 +1,39 @@
 package alura.orcamentofamiliar.receita.adapter.in.web;
 
 import alura.orcamentofamiliar.receita.adapter.in.web.response.ReceitaResponse;
-import alura.orcamentofamiliar.receita.application.port.out.FindTodasAsReceitasPelaDescricaoPort;
 import alura.orcamentofamiliar.receita.application.port.out.FindAllReceitasPort;
+import alura.orcamentofamiliar.receita.application.port.out.FindTodasAsReceitasPelaDescricaoPort;
 import alura.orcamentofamiliar.receita.domain.Receita;
-import alura.orcamentofamiliar.util.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/orcamento-familiar")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ListarTodasAsReceitasController {
 
-    private final FindAllReceitasPort findAllReceitasPort;
     private final FindTodasAsReceitasPelaDescricaoPort findTodasAsReceitasPelaDescricaoPort;
+    private final FindAllReceitasPort findAllReceitasPort;
 
     @GetMapping("/receitas/listar-todas")
-    public ResponseEntity<List<ReceitaResponse>> execute(@RequestParam(required = false) String descricao) {
+    public ResponseEntity<Page<ReceitaResponse>> execute(@RequestParam(required = false) String descricao,
+                                                         Pageable paginacao) {
 
-            List<Receita> receitas = descricao == null ? findAllReceitasPort.findAllReceitas()
-                                                       :
-                                     findTodasAsReceitasPelaDescricaoPort.listaTodasAsReceitasPorDescricao(
-                                                               descricao);
+        Page<Receita> receitas = descricao == null ? findAllReceitasPort.findAllReceitas(paginacao)
+                                                   :
+                                 findTodasAsReceitasPelaDescricaoPort.listaTodasAsReceitasPorDescricao(
+                                                           descricao,
+                                                           paginacao);
 
-            return ResponseEntity.ok()
-                                 .body(ReceitaResponse.from(receitas));
-
+        return ResponseEntity.ok()
+                             .body(ReceitaResponse.fromPage(receitas));
 
     }
 
